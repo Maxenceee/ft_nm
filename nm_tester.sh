5dfa8be4ae2520e6557eb8b9e2e02bc9117fe119
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2025-07-01 11:02:39 +0200

    Update nm_tester.sh

--- nm_tester.sh
+++ nm_tester.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "${YELLOW}Starting ft_nm vs nm comparison tests...${NC}"
+echo -e "${YELLOW}Starting ft_nm vs nm comparison tests...${NC}"
 
 BIN=("btc" "marvin_game" "rpn" "taskmasterd" "woody" "famine" "PmergeMe" "target" "webserv" "woody_woodpacker")
 FLAGS=("" "-a" "-g" "-u")
@@ -12,18 +12,18 @@ NC='\033[0m' # No Color
 for T in "${BIN[@]}"; do
 	TARGET=./targets/$T
 	for F in "${FLAGS[@]}"; do
-		echo "${YELLOW}Testing $T with flag '$F'...${NC}"
+		echo -e "${YELLOW}Testing $T with flag '$F'...${NC}"
 		./ft_nm $F "$TARGET" 2>&1 | sort > out.f
 		nm $F "$TARGET" 2>&1 | sort > out.m
 
 		diff out.f out.m > out.diff
 		if [ $? -ne 0 ]; then
-			echo "${RED}Difference found for $T with flag '$F':${NC}"
-			echo "./ft_nm $F "$TARGET" 2>&1 | sort > out.f"
+			echo -e "${RED}Difference found for $T with flag '$F':${NC}"
+			echo -e "./ft_nm $F "$TARGET" 2>&1 | sort > out.f"
 			cat out.diff
 			exit
 		else
-			echo "${GREEN}No difference for $T with flag '$F'${NC}"
+			echo -e "${GREEN}No difference for $T with flag '$F'${NC}"
 		fi
 	done
 done
@@ -35,22 +35,22 @@ for T in "${BIN[@]}"; do
 		for F2 in "${FLAGS[@]}"; do
 			if [ "$F1" != "$F2" ]; then
 				COMBO="$F1 $F2"
-				echo "${YELLOW}Testing $T with flags '$COMBO'...${NC}"
+				echo -e "${YELLOW}Testing $T with flags '$COMBO'...${NC}"
 				./ft_nm $COMBO "$TARGET" 2>&1 | sort > out.f
 				nm $COMBO "$TARGET" 2>&1 | sort > out.m
 
 				diff out.f out.m > out.diff
 				if [ $? -ne 0 ]; then
-					echo "${RED}Difference found for $T with flags '$COMBO':${NC}"
-					echo "./ft_nm $COMBO \"$TARGET\" 2>&1 | sort > out.f"
+					echo -e "${RED}Difference found for $T with flags '$COMBO':${NC}"
+					echo -e "./ft_nm $COMBO \"$TARGET\" 2>&1 | sort > out.f"
 					cat out.diff
 					exit
 				else
-					echo "${GREEN}No difference for $T with flags '$COMBO'${NC}"
+					echo -e "${GREEN}No difference for $T with flags '$COMBO'${NC}"
 				fi
 			fi
 		done
 	done
 done
 
-echo "${YELLOW}All tests completed.${NC}"
+echo -e "${YELLOW}All tests completed.${NC}"
